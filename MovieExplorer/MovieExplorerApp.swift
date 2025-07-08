b70b903e4e4cf551a2fcda40c19cc9b1315f07fd
MujtabaMK <mujtabakhanakola@gmail.com>
2025-07-08 16:16:20 +0530

    Initial Commit

---
+++ MovieExplorer/MovieExplorerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieExplorerApp.swift
+//  MovieExplorer
+//
+//  Created by Mujtaba Khan on 08/07/25.
+//
+
+import SwiftUI
+
+@main
+struct MovieExplorerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
